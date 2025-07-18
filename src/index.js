09f6534db12196ed54646fd57377274426064e14
VYOM PATEL <124497900+Vyom1912@users.noreply.github.com>
2025-07-18 19:55:01 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,22 +4,17 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import ShopContextProvider from "./Context/ShopContext";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ShopContextProvider>
-     <HashRouter>
+      <HashRouter>
         <App />
       </HashRouter>
     </ShopContextProvider>
   </React.StrictMode>
-  
-</HashRouter>
-
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
